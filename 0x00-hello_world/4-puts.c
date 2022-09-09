9bb0274df196af2a0c170234f4fe00d88aeceea7
Enosolease M. Ame <enosmath@gmail.com>
2022-09-08 20:02:54 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,17 @@
 /**
  *  main - Print a string to stdout
  *
+ *  Description: This function does some awesome stuff
  *  RETURN: The integer denotig the success of failure of puts. 0 (Success)
- *  */
+ */
 
 int main(void)
 {
 	/**
 	 * @c: return value from puts function. Boolean for success or failure
-	 * */
+	 */
 	int c;
 	c = puts("\"Programming is like building a multilingual puzzle");
-	return (c);
+	return (0);
 }
+
