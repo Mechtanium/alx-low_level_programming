27158f260feff230b3670a233d3e5afb9ccf15f2
Enosolease M. Ame <enosmath@gmail.com>
2022-09-12 15:37:20 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,9 +13,9 @@
 
 int main(void)
 {
-	char msg[] ="and that piece of art is useful\" - \
+	char *msg[] ="and that piece of art is useful\" - \
 		Dora Korpar, 2015-10-19";
-	fwrite(*msg, sizeof(msg), 1, stderr);
+	fwrite(&msg, sizeof(msg), 1, stderr);
 	return (0);
 }
 
