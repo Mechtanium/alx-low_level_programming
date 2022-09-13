50800683dd055e43b18ae238f57dcb964474e7c6
Enosolease M. Ame <enosmath@gmail.com>
2022-09-13 14:07:13 -0700

    Write a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 #include "main.h"
 #include <stdio.h>
 #include "_putchar.c"
+
 /**
  * print_alphabet - writes to standard output
  *
  * Description: Doesn't use the standard library. Uses fwrite.
  * Result: 0 (success) and 1 (failure)
  */
-
 void print_alphabet(void);
 
 /**
@@ -23,19 +23,3 @@ int main(void)
 	return (0);
 }
 
-/**
- * print_alphabet - writes to standard output
- *
- * Description: Doesn't use the standard library. Uses fwrite.
- * Result: 0 (success) and 1 (failure)
- */
-void print_alphabet()
-{
-        char str[] = "abcdefghijklmnopqrstuvwxyz\n";
-        int i;
-
-        for(i = 0; i <= 26; i++)
-        {
-                _putchar(str[i]);
-        }
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,3 +19,20 @@ void simple_write(char text[], int len)
 	}
 }
 
+/**
+ * print_alphabet - writes to standard output
+ *
+ * Description: Doesn't use the standard library. Uses fwrite.
+ * Result: 0 (success) and 1 (failure)
+ */
+void print_alphabet(void)
+{
+        char str[] = "abcdefghijklmnopqrstuvwxyz\n";
+        int i;
+
+        for(i = 0; i <= 26; i++)
+        {
+                _putchar(str[i]);
+        }
+}
+
