c04361e64db0ea5de48894ad8331f684e3a5d324
Enosolease M. Ame <enosmath@gmail.com>
2022-09-16 19:29:25 -0700

    Write a function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Read to learn
+ *
+ * Description: Write a function that draws a straight line in the terminal.
+ * Return: 0 (success) and 1 (failure) to execute
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
