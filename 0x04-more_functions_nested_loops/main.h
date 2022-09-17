93bd1af9da74b553e6c2d7f592d642aa20e6dbcb
Enosolease M. Ame <enosmath@gmail.com>
2022-09-16 17:29:20 -0700

    Write a function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H //You may change this (e.g. main.h becomes MAIN_H)
-#define MAIN_H //You may change this (e.g. main.h becomes MAIN_H)
+#ifndef MAIN_H /*You may change this (e.g. main.h becomes MAIN_H)*/
+#define MAIN_H /*You may change this (e.g. main.h becomes MAIN_H)*/
 int _isupper(int c);
 #endif
