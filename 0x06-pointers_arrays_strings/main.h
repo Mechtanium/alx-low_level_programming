0ca17f2618f0586d93aa99eb4e194ef6bee5edad
Enosolease M. Ame <enosmath@gmail.com>
2022-09-21 15:17:29 -0700

    Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 #endif
