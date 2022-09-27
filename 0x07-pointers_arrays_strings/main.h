0403a78fe423ff812199a3cbed1dcb7971c5c38f
Enosolease M. Ame <enosmath@gmail.com>
2022-09-26 17:21:55 -0700

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H) */
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
