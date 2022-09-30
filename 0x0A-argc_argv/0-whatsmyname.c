ce50184e0d164b1cfa2249e3a1b081f52914becd
Enosolease M. Ame <enosmath@gmail.com>
2022-09-30 16:40:51 -0700

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print function name.
+ * @argc: Variable count
+ * @argv: Variable pointer array
+ *
+ * Description: Prints the name of the program that called this code.
+ * Return: EXIT_SUCCESS.
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (EXIT_SUCCESS);
+}
