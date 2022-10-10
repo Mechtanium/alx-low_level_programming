d2a2f2c23df110a02678d62bdcdc560da3f73bd6
Enosolease M. Ame <enosmath@gmail.com>
2022-10-10 15:23:54 -0700

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,18 @@
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 int _putchar(char c); /* For testing. Don't delete */
 
-
+/**
+ * dog - defines a struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: defines a struct dog
+ */
 struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
