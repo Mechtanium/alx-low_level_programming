0a1d773b1db3917deeaa90f6d07dcb355f9dda1c
Enosolease M. Ame <enosmath@gmail.com>
2022-10-11 18:02:51 -0700

    Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,5 @@
 #ifndef PI_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 #define PI_H /* You may change this (e.g. main.h becomes MAIN_H)*/
-int _putchar(char c); /* For testing. Don't delete */
 
 #define PI 3.14159265359
 #endif
