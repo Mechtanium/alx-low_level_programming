8fb3d1730e6919712f67a5fcebfb6f497540f9b4
Enosolease M. Ame <enosmath@gmail.com>
2022-10-14 16:44:59 -0700

    Write a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,54 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - Print numbers
+ * printer - Switches to print type
+ * @format: String of format characters
+ * @: Integer counter
+ * @ls: va_list list of variable parameters.
+ *
+ * Description: Switches between c, i, f, s data types
+ * Return:Void (Nothing).
+ */
+void printer(const char * const format, unsigned int i, va_list ls)
+{
+	char *s;
+
+	switch (format[i])
+	{
+		case 'c':
+			if (i)
+				printf(", ");
+			printf("%c", va_arg(ls, int));
+			break;
+		case 'i':
+			if (i)
+				printf(", ");
+			printf("%d", va_arg(ls, int));
+			break;
+		case 'f':
+			if (i)
+				printf(", ");
+			printf("%f", va_arg(ls, double));
+			break;
+		case 's':
+			if (i)
+				printf(", ");
+
+			s = va_arg(ls, char *);
+
+			if (s != NULL)
+				printf("%s", s);
+			else
+				printf("(nil)");
+			break;
+		default:
+
+			break;
+	}
+}
+
+/**
+ * print_all - Print numbers
  * @format: Char pointer to format string
  *
  * Description: Print each variable argument element with separator between.
@@ -14,7 +61,6 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i = 0, n = 0;
 	va_list ls;
-	char *s;
 
 	va_start(ls, format);
 
@@ -26,37 +72,8 @@ void print_all(const char * const format, ...)
 
 	while (n && format[i])
 	{
-		switch (format[i])
-		{
-			case 'c':
-				if (i)
-					printf(", ");
-				printf("%c", va_arg(ls, int));
-				break;
-			case 'i':
-				if (i)
-					printf(", ");
-				printf("%d", va_arg(ls, int));
-				break;
-			case 'f':
-				if (i)
-					printf(", ");
-				printf("%f", va_arg(ls, double));
-				break;
-			case 's':
-				if (i)
-					printf(", ");
-				s = va_arg(ls, char *);
+		printer(format, i, ls);
 
-				if (s != NULL)
-					printf("%s", s);
-				else
-					printf("(nil)");
-				break;
-			default:
-
-				break;
-		}
 		i++;
 		n--;
 	}
