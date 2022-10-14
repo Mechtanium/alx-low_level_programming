906e40a9eddf62f8b517677f642d52c4dad2d3d4
Enosolease M. Ame <enosmath@gmail.com>
2022-10-14 12:34:13 -0700

    Write a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 int _putchar(char c); /* For testing. Don't delete */
-/* Code here */
+int sum_them_all(const unsigned int n, ...);
 #endif
