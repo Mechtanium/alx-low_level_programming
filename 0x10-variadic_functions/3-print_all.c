1dcce59701651b0a00246af39523d2d1b520b421
Enosolease M. Ame <enosmath@gmail.com>
2022-10-14 17:06:58 -0700

    Write a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,53 +4,6 @@
 #include "variadic_functions.h"
 
 /**
- * printer - Switches to print type
- * @format: String of format characters
- * @i: Integer counter
- * @ls: va_list list of variable parameters.
- *
- * Description: Switches between c, i, f, s data types
- * Return:Void (Nothing).
- */
-void printer(const char * const format, unsigned int i, va_list ls)
-{
-	char *s;
-
-	switch (format[i])
-	{
-		case 'c':
-			if (i)
-				printf(", ");
-			printf("%c", va_arg(ls, int));
-			break;
-		case 'i':
-			if (i)
-				printf(", ");
-			printf("%d", va_arg(ls, int));
-			break;
-		case 'f':
-			if (i)
-				printf(", ");
-			printf("%f", va_arg(ls, double));
-			break;
-		case 's':
-			if (i)
-				printf(", ");
-
-			s = va_arg(ls, char *);
-
-			if (s != NULL)
-				printf("%s", s);
-			else
-				printf("(nil)");
-			break;
-		default:
-
-			break;
-	}
-}
-
-/**
  * print_all - Print numbers
  * @format: Char pointer to format string
  *
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 #define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
 int _putchar(char c); /* For testing. Don't delete */
@@ -5,4 +6,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void printer(const char * const format, unsigned int i, va_list ls);
 #endif
