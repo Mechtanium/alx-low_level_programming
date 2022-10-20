4c9d89af9d4d5b861631bb867a9a631b5c5cd51e
Enosolease M. Ame <enosmath@gmail.com>
2022-10-20 10:39:21 -0700

    Write a function that prints all the elements of a `list_t` list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include "lists.h"
 
 /**
  * main - 
@@ -9,7 +9,25 @@
  * Description: 
  * Return: 
  */
-int main(char argc)
+size_t print_list(list_t *h)
 {
-	return (EXIT_SUCCESS);
+	size_t count = 0;
+	list_t *ptr = h;
+
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	do
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+		count++;
+	}while (ptr != NULL);
+	return (count);
 }
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
+#define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
+int _putchar(char c); /* For testing. Don't delete */
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	    char *str;
+	        unsigned int len;
+		    struct list_s *next;
+} list_t;
+
+size_t print_list(list_t *h);
+#endif
