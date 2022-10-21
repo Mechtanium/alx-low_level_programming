208d025a835c0a948e568fd53f524b911aaa7d33
Enosolease M. Ame <enosmath@gmail.com>
2022-10-20 21:02:26 -0700

    Write a 64-bit program in assembly that prints `Hello, Holberton`, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,17 @@
-global main
-extern printf
-
-section .text
-main:
-    push rbp
-    mov rbp, rsp
-
-
-    ;function body
-    xor eax, eax
-    lea rdi, [msg]
-    call printf
-
-
-    add rsp, 16
-    leave
-    ret
-
 section .data
-    msg: db "Hello, Holberton", 0
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
 
-section .bss
+	section .text
+	extern printf
+	global main
+
+main:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
