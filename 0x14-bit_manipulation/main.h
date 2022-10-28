85a213bbf78101c50cbc24f961836057277d7490
Enosolease M. Ame <enosmath@gmail.com>
2022-10-27 19:31:36 -0700

    Write a function that converts a binary number to an `unsigned int`.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
-#define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
+#ifndef MAIN_H 
+#define MAIN_H 
 int _putchar(char c); /* For testing. Don't delete */
 unsigned int binary_to_uint(const char *b);
 #endif
