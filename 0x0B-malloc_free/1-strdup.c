32776d66ff3fda9a88567a4aed1218c163c79013
Enosolease M. Ame <enosmath@gmail.com>
2022-11-16 12:06:20 -0800

    Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - Copies strings
+ * @str: Old string
+ *
+ * Description: Copies a strings.
+ * Return: Pointer to new string memory.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	char *new_str;
+
+	if (!str)
+		return ((char *) NULL);
+
+	while (str[i])
+		i++;
+
+	new_str = malloc(sizeof(char) * i);
+
+	if (!new_str)
+		return ((char *) NULL);
+
+	i = 0;
+	while (str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = '\0';
+
+	return (new_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
-#define MAIN_H /* You may change this (e.g. main.h becomes MAIN_H)*/
-int _putchar(char c); /* For testing. Don't delete */
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+/* For testing. Don't delete */
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
