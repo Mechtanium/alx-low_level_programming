5c3909d2bdeebdc35b9655f179e4041faa465611
Enosolease M. Ame <enosmath@gmail.com>
2022-11-17 23:21:32 -0800

    Write a function that allocates memory using `malloc`.

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define EXIT_FAIL 98
+/**
+ * _putchar - Don't delete _putchar, its used for testing
+ * @c: character to be printed
+ *
+ * Description: It doesn't affect compilation or checker testing but will crash
+ * the ALX tool tester if removed.
+ * Return: 0 (Success) or 1 (Failure).
+ */
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
